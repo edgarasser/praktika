f43e6bbc6b3822cd0485d1a8c8cae3240ceefbd5
unknown <edgaras.serzentas@stud.vgtu.lt>
2019-08-13 15:26:38 +0300

    08-13

---
+++ learning_log/mypython/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\edgaras\desktop\stuff-master\learning_log\mypython\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
